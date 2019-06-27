4df20e6fe1e72e55703b32b627cdc3784044440f
Muata Nkosi <muata.nkosi@prudential.com>
2019-06-27 10:04:58 -0400

    Add variable before test

--- Interview Questions/chapter01/test.spec.js
+++ Interview Questions/chapter01/test.spec.js
@@ -1,3 +1,5 @@
+var hello = 5;
+
 describe('Hello World Server ', () => {
     it('should equal one', () => {
         expect(1).toBe(1);
@@ -5,4 +7,7 @@ describe('Hello World Server ', () => {
     it('should not equal one', () => {
         expect(1).toBeLessThan(2);
     });
+    it('should not equal hello', () => {
+        expect(1).toBe(hello);
+    })
 });
