ab08ef798ba24ec4e40b4cdb3451d0678235bbb0
Muata Nkosi <muatankosi1@gmail.com>
2021-04-13 07:26:03 -0400

    Add solution for forward linked lists for 2.5sumLists

--- Interview_Questions/chapter02/2.5sumLists.js
+++ Interview_Questions/chapter02/2.5sumLists.js
@@ -1,5 +1,6 @@
 const Iterator = require('../../utils/Iterator');
 const LinkedList = require('../../utils/LinkedList');
+const Stack = require('../../utils/Stack');
 
 // Sum Lists: You have two numbers represented by a linked list, 
 // where each node contains a single digit.
@@ -29,7 +30,37 @@ const sumListsReverse = (list1, list2) => {
 
 const sumLists = (list1, list2) => {
     let iter1 = new Iterator(list1), iter2 = new Iterator(list2);
-    
+    const stack1 = new Stack(), stack2 = new Stack();
+    while (iter1.hasNode() || iter2.hasNode()) {
+        if (iter1.hasNode()) {
+            stack1.push(iter1.getData());
+            iter1.getNext();
+        }
+        if (iter2.hasNode()) {
+            stack2.push(iter2.getData());
+            iter2.getNext();
+        }
+    }
+    let exp = -1, acc = 0;
+    while (!stack1.isEmpty() || !stack2.isEmpty()) {
+        let curr = 0;
+        exp+=1;
+        if (!stack1.isEmpty()) {
+            curr += stack1.pop();
+        }
+        if (!stack2.isEmpty()) {
+            curr += stack2.pop();
+        }
+        curr = curr * Math.pow(10, exp);
+        acc += curr;
+    }
+    let list3 = new LinkedList();
+    while (acc > 0) {
+        list3.add(Math.floor(acc/Math.pow(10, exp)));
+        acc = acc % Math.pow(10, exp);
+        exp -=1;
+    }
+    return list3;
 }
 
 
--- spec/chapter02/2.5sumLists.spec.js
+++ spec/chapter02/2.5sumLists.spec.js
@@ -32,26 +32,26 @@ describe('Sum Lists', () => {
         it('should add numbers reversed as linked lists.', () => {
             let list1 = new LinkedList();
             let list2 = new LinkedList();
-            list1.add(7);
-            list1.add(1);
             list1.add(6);
-            list2.add(5);
-            list2.add(9);
+            list1.add(1);
+            list1.add(7);
             list2.add(2);
-            let list3 = sumListsReverse(list1, list2);
+            list2.add(9);
+            list2.add(5);
+            let list3 = sumLists(list1, list2);
             expect(list3.show()).toEqual([9,1,2]);
         });
     
         it('should add numbers reversed as linked lists of different lengths.', () => {
             let list1 = new LinkedList();
             let list2 = new LinkedList();
-            list1.add(7);
             list1.add(1);
-            list2.add(5);
-            list2.add(9);
-            list2.add(2);
+            list1.add(7);
             list2.add(4);
-            let list3 = sumListsReverse(list1, list2);
+            list2.add(2);
+            list2.add(9);
+            list2.add(5);
+            let list3 = sumLists(list1, list2);
             expect(list3.show()).toEqual([4,3,1,2]);
         });
     });
