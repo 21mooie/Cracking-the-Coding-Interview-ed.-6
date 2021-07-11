bf053ee4ce643d07e7c3a1f048b967c2b5b2fab4
Muata Nkosi <muatankosi1@gmail.com>
2021-07-11 17:50:57 -0400

    Implement 5.8drawLine I didn't solve it but I would like to move on from binary questions

---
+++ Interview_Questions/chapter05/5.8drawLine.js
@@ -0,0 +1,13 @@
+// Draw Line: A monochrome screen is stored as a single array of bytes,
+// allowing eight consecutive pixels to be stored in one byte.
+// The screen has width w, where wis divisible by 8 (that is, no byte will be split across rows).
+// The height of the screen, of course, can be derived from the length of the array andthewidth.
+// Implement a function that draws a horizontal line from( xl, y) to (x2, y).
+// The method signature should look something like:
+// drawLine(byte[] screen, int width, int xl, int x2, int y)
+
+const drawLine = (screen, width, x1, x2, y) => {
+    return 'hello world';
+};
+
+module.exports = drawLine;
---
+++ spec/chapter05/5.8drawLine.spec.js
@@ -0,0 +1,6 @@
+describe('Draw Line', () => {
+    it('should draw a line from point 1 to point 2.', () => {
+        let screen = [];
+        let byte = 01010101;
+    });
+});
