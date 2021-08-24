17997fa54e58ceeda88bd16a97cadc9997be90fa
Muata Nkosi <muatankosi1@gmail.com>
2021-08-24 16:29:28 -0400

    Ensure tests work for permutations with dups

--- spec/chapter08/8.8permutationsWithDups.spec.js
+++ spec/chapter08/8.8permutationsWithDups.spec.js
@@ -2,6 +2,13 @@ const permutationsWithDups = require("../../Interview_Questions/chapter08/8.8per
 
 describe('Permutations With Duplicates', () => {
     it('should return all the permutations without duplicates.', () => {
-        console.log(permutationsWithDups('abca'));
+        expect(permutationsWithDups('abca')).toEqual([
+            'abca', 'baca',
+            'bcaa', 'acba',
+            'caba', 'cbaa',
+            'acab', 'caab',
+            'abac', 'baac',
+            'aabc', 'aacb'
+          ]);
     });
 });
